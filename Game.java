5d9849587106a7608056e6cd6de984ed5e0c8478
Anton <anton.goretsky@gmail.com>
2016-01-20 11:43:12 -0500

    Added basic Game.java

---
+++ Game.java
@@ -0,0 +1,14 @@
+public class Game {
+	
+	private int currentLine;
+	private int currentRoom;
+	
+	public static play() {
+		System.out.println("Passing Compiler");
+	}
+	
+	public static void main(String[] args) {
+		play()
+	}
+	
+}
