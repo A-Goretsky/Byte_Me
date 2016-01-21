c3eb9c5fd6d94ec95a4ce5c7b1556482515f51b3
Anton <anton.goretsky@gmail.com>
2016-01-20 23:42:51 -0500

    Send Dylan Game for bugfix

--- Game.java
+++ Game.java
@@ -1,19 +1,18 @@
 import java.io.*;
 import java.util.ArrayList;
+import cs1.Keyboard;
 
 public class Game {
 	
 	private static int currentLine = 0;
 	private static int currentRoom = 0;
-	private static boolean inFight = false;
+	private static boolean inFight = true;
 	private static ArrayList story = new ArrayList();
+	private static ArrayList storyTemp = new ArrayList();
+	private static int characterType;
 	
-<<<<<<< HEAD
 	//every single story line will contain a 4 character precursor <room>,<line>,<battle>,---random battle chance<true or false>---
 	//true or false corresponds to the inFight boolean.
-=======
-	//every single story line will contain a 2 character precursor <room>,<line>
->>>>>>> f9b03bca2d133449aba931e2c4be9cbe773bb852
 	
 	private static void interpreter(String input) {
 		if (inFight) {
@@ -21,12 +20,12 @@ public class Game {
 				//try switch method here
 				case "map": 
 					System.out.println("Access to map is not permitted during a battle.");
+					interpreter(Keyboard.readString());
 					break;
-					//interpreter(CHANGE TO KEYBOARD input);
 				case "log": 
 					System.out.println("Access to the log is not permitted during a battle.");
+					interpreter(Keyboard.readString());
 					break;
-					//interpreter(//CHANGE TO KEYBOARD input);
 				default:
 					//passing compiler
 					//Battle.playerTurn(input);
@@ -45,16 +44,37 @@ public class Game {
 	}
 	
 	private static void map() {
+		System.out.println("Map function not yet implemented.");
 		System.out.println("Passing Compiler");
 		//Will use the currentLine and currentRoom to determine where to place character in map.
 	}
 	
 	private static void log() {
+		System.out.println("Log function not yet implemented.");
 		System.out.println("Passing Compiler");
 	}
 	
 	public static void play() {
+		String input;
+		input = Keyboard.readString();
 		System.out.println(story);
+		System.out.println(input);
+		interpreter(input);
+	}
+	
+	private static void separateStory() {
+		String line = "";
+		String[] temp = new String[4];
+		String vars = "";
+		for (String lineTemp : storyTemp) {
+			//var setting not right here.
+			temp = lineTemp.split($);
+			currentLine = temp[0];
+			currentRoom = temp[1];
+			inFight = temp[2];
+			line = temp[3];
+			story.add(line);
+		}
 	}
 	
 	private static void readFile() {
@@ -63,7 +83,8 @@ public class Game {
 			FileReader file = new FileReader("story.txt");
 			BufferedReader bufferedFile = new BufferedReader(file);
 			while ((line = bufferedFile.readLine()) != null) {
-				story.add(line);
+				storyTemp.add(line);
+				separateStory();
 			}
 		}
 		catch (IOException e) {
